dbe77ae87601b7756be722c7c91fd8fbe4887cb1
vuvansy <vuvansy3001@gmail.com>
2023-12-20 00:59:35 +0700

    5.2 Định nghĩa route có tham số

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Định tuyến cơ bản
+//=== 5.1 Định tuyến cơ bản ===
 // domain.com/post 
 
 Route::get('/post', function () {
@@ -32,4 +32,17 @@ Route::get('/admin/product', function () {
 //Đường dẫn dành riêng cho Admin thêm sản phẩm
 Route::get('/admin/product/add', function () {
     return "Trang thêm mới sản phẩm";
+});
+
+
+//=== 5.2 Định nghĩa route có tham số (vd: id...) ===
+
+Route::get('posts/{id}', function ($id) {
+    return $id;
+});
+
+
+// VD: về route có tham số đối với phân trang bài viết
+Route::get('posts/{cat_id}/page/{page}', function ($cat_id, $page) {
+    return $cat_id . '-' . $page;
 });
